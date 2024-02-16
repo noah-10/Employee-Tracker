84abddd1e6ba5dd91ca94515919f991a7240239b
noah-10 <noah.fryman.10@gmail.com>
2024-02-16 12:25:02 -0500

    reorganized files and folders & added comments

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const inquirer = require("inquirer");
-const CheckResponse = require("./lib/cli_response.js");
 const db = require("./config/config.js");
-const menu = require("./lib/mainMenu.js");
+const menu = require("./lib/menu/mainMenu.js");
 
 db.connect((err) => {
     if(err){
@@ -13,46 +11,4 @@ db.connect((err) => {
 
 global.db = db;
 
-// class Cli {
-    
-//     run(){
-//         //Prompt questions
-//         inquirer
-
-//         .prompt([
-//         {
-//           type: "list",
-//           name: "db_options",
-//           message: "What would you like to do?",
-//           choices:[ "View All Employees",
-//                     "Add Employee",
-//                     "Update Employee Role",
-//                     "View All Roles",
-//                     "Add Role",
-//                     "View All Departments",
-//                     "Add Department" ]  
-//         }
-//         ])
-//         .then((response) => {
-//             // save response to variable
-//             const userOption = response.db_options;
-            
-//             // Run a new check response to decide what sql query to render
-//             new CheckResponse().render(userOption);
-
-//         });
-//     };
-    
-// };
-
-// const init = () => {
-
 menu.mainMenu();
-    // const newInstance = new Cli;
-
-    // newInstance.run();
-// }
-
-// init();
-
-// module.exports = Cli;
